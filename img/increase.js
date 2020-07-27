361918608f48ade740bae084e714814708f6de48
FarrukhKhan7 <58085304+FarrukhKhan7@users.noreply.github.com>
2020-07-27 20:32:02 +0500

    Add files via upload

---
+++ img/increase.js
@@ -0,0 +1,125 @@
+function zoomin() { 
+    var GFG = document.getElementById("geeks"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth + 100) + "px"; 
+
+var GFG = document.getElementById("new"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth + 100) + "px";
+
+var GFG = document.getElementById("newest"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth + 100) + "px";
+
+var GFG = document.getElementById("newestlast"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth + 100) + "px";
+
+var GFG = document.getElementById("next"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth + 100) + "px";
+
+var GFG = document.getElementById("1"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth + 100) + "px";
+
+var GFG = document.getElementById("2"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth + 100) + "px";
+
+var GFG = document.getElementById("3"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth + 100) + "px";
+
+var GFG = document.getElementById("4"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth + 100) + "px";
+
+var GFG = document.getElementById("5"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth + 100) + "px";
+
+var GFG = document.getElementById("6"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth + 100) + "px";
+
+var GFG = document.getElementById("7"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth + 100) + "px";
+
+var GFG = document.getElementById("8"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth + 100) + "px";
+
+var GFG = document.getElementById("9"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth + 100) + "px";
+
+var GFG = document.getElementById("10"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth + 100) + "px";
+  } 
+    
+  function zoomout() { 
+    var GFG = document.getElementById("geeks"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth - 100) + "px"; 
+
+var GFG = document.getElementById("new"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth - 100) + "px";
+
+var GFG = document.getElementById("newest"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth - 100) + "px";
+
+var GFG = document.getElementById("newestlast"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth - 100) + "px";
+
+var GFG = document.getElementById("next"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth - 100) + "px";
+
+var GFG = document.getElementById("1"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth - 100) + "px";
+
+var GFG = document.getElementById("2"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth - 100) + "px";
+
+var GFG = document.getElementById("3"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth - 100) + "px";
+
+var GFG = document.getElementById("4"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth - 100) + "px";
+
+var GFG = document.getElementById("5"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth - 100) + "px";
+
+var GFG = document.getElementById("6"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth - 100) + "px";
+
+var GFG = document.getElementById("7"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth - 100) + "px";
+
+var GFG = document.getElementById("8"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth - 100) + "px";
+
+var GFG = document.getElementById("9"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth - 100) + "px";
+
+var GFG = document.getElementById("10"); 
+    var currWidth = GFG.clientWidth; 
+    GFG.style.width = (currWidth + 100) - "px";
+
+
+  } 
